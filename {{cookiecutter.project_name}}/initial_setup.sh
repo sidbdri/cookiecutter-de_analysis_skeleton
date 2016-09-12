b8a69d70cf839ed14b239c7ce60533aa80d6d316
Owen Dando <owen.dando@ed.ac.uk>
2016-09-12 15:34:03 +0100

    Attempt to setup up virtual env correctly.

--- {{cookiecutter.project_name}}/initial_setup.sh
+++ {{cookiecutter.project_name}}/initial_setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+export WORKON_HOME={{cookiecutter.virtualenv_home}}
+export PROJECT_HOME={{cookiecutter.projects_base}}
+source /usr/local/bin/virtualenvwrapper.sh
+
 mkproject -f {{cookiecutter.project_name}}
+
 pip install multiqc

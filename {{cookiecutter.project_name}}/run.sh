bd9139eaa5a448356a8880acf94b72ddc7c0246d
katieemelianova <katie_emelianova@hotmail.com>
2020-06-05 12:53:18 +0100

    complete sargasso option pipeline

--- {{cookiecutter.project_name}}/run.sh
+++ {{cookiecutter.project_name}}/run.sh
@@ -1,9 +1,8 @@
 
 {% if cookiecutter.sargasso == "yes" %}
-snakemake -s Snakefile.run_analysis sargasso 
-{% endif %}
-
-
+snakemake -s Snakefile.run_analysis bams_mixed_species
+{% else %}
 snakemake -s Snakefile.run_analysis bams
+{% endif %}
 snakemake -s Snakefile.run_analysis  multiqc
 

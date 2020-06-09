a882cccdcf0d90032274e58b5c7d08558ef304dc
katieemelianova <katie_emelianova@hotmail.com>
2020-06-09 08:44:29 +0100

    split bams and the remaining analyses into two rules to allow strand test to work

--- {{cookiecutter.project_name}}/run.sh
+++ {{cookiecutter.project_name}}/run.sh
@@ -1,7 +1,9 @@
 
 {% if cookiecutter.sargasso == "yes" %}
+snakemake -s Snakefile.multispecies_analysis bams
 snakemake -s Snakefile.multispecies_analysis multiqc
 {% else %}
+snakemake -s Snakefile.singlespecies_analysis  bams
 snakemake -s Snakefile.singlespecies_analysis  multiqc
 {% endif %}
 

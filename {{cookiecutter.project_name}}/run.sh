2b05f5e0bf84b509b130ffdf37885735e0a5c278
Katie Emelianova <kemelianova@sidbdri.inf.ed.ac.uk>
2020-08-19 15:59:57 +0100

    give only -j flag when calling snakemake

--- {{cookiecutter.project_name}}/run.sh
+++ {{cookiecutter.project_name}}/run.sh
@@ -1,14 +1,12 @@
 
-NUM_THREADS_PER_SAMPLE={{cookiecutter.number_threads_per_sample}}
 NUM_TOTAL_THREADS={{cookiecutter.number_total_threads}}
-NUM_PARALLEL_JOBS=`expr ${NUM_TOTAL_THREADS} / ${NUM_THREADS_PER_SAMPLE}`
 
 {% if cookiecutter.sargasso == "yes" %}
-snakemake -s Snakefile.multispecies_analysis bams -j $NUM_PARALLEL_JOBS
-snakemake -s Snakefile.multispecies_analysis multiqc -j $NUM_PARALLEL_JOBS
+snakemake -s Snakefile.multispecies_analysis bams -j $NUM_TOTAL_THREADS
+snakemake -s Snakefile.multispecies_analysis multiqc -j $NUM_TOTAL_THREADS
 {% else %}
-snakemake -s Snakefile.singlespecies_analysis  bams -j $NUM_PARALLEL_JOBS
-snakemake -s Snakefile.singlespecies_analysis  multiqc -j $NUM_PARALLEL_JOBS
+snakemake -s Snakefile.singlespecies_analysis  bams -j $NUM_TOTAL_THREADS
+snakemake -s Snakefile.singlespecies_analysis  multiqc -j $NUM_TOTAL_THREADS
 {% endif %}
 
 

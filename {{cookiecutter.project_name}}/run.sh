0d83b42a3479dfa9cb330f536edbde1da38fa3dc
Katie Emelianova <kemelianova@sidbdri.inf.ed.ac.uk>
2020-08-19 13:21:36 +0100

    replicate parallel in snakemake

--- {{cookiecutter.project_name}}/run.sh
+++ {{cookiecutter.project_name}}/run.sh
@@ -1,10 +1,14 @@
 
+NUM_THREADS_PER_SAMPLE={{cookiecutter.number_threads_per_sample}}
+NUM_TOTAL_THREADS={{cookiecutter.number_total_threads}}
+NUM_PARALLEL_JOBS=$(awk '{print int($1/$2)}' <<< "${NUM_TOTAL_THREADS} ${NUM_THREADS_PER_SAMPLE}")
+
 {% if cookiecutter.sargasso == "yes" %}
-snakemake -s Snakefile.multispecies_analysis bams
-snakemake -s Snakefile.multispecies_analysis multiqc
+snakemake -s Snakefile.multispecies_analysis bams -j $NUM_PARALLEL_JOBS
+snakemake -s Snakefile.multispecies_analysis multiqc -j $NUM_PARALLEL_JOBS
 {% else %}
-snakemake -s Snakefile.singlespecies_analysis  bams
-snakemake -s Snakefile.singlespecies_analysis  multiqc
+snakemake -s Snakefile.singlespecies_analysis  bams -j $NUM_PARALLEL_JOBS
+snakemake -s Snakefile.singlespecies_analysis  multiqc -j $NUM_PARALLEL_JOBS
 {% endif %}
 
 

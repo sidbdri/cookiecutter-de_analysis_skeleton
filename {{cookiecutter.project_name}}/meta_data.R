f9d850cd02bcb9532668883397f638bdd11455d3
katieemelianova <katie_emelianova@hotmail.com>
2021-02-10 11:34:49 +0000

    revert changes

--- {{cookiecutter.project_name}}/meta_data.R
+++ {{cookiecutter.project_name}}/meta_data.R
@@ -128,11 +128,6 @@ MISASSIGNMENT_SAMPLE_REFERENCE_TABLE <- tribble(
 FEATURES_FOR_ALL_SAMPLES_PCA <- c('condition')
 FEATURES_FOR_ALL_SAMPLES_HEATMAP <- c('condition', 'treatment')
 
-# Set up celltype graph features
-# The following two vectors must be the same length
-CELLTYPE_FEATURE_GROUP <- c('celltype')
-CELLTYPE_PLOT_FEATURE <- c('color')
-
 SUMMARY_TB <- setNames(data.frame(matrix(ncol = 14, nrow = 0)),
                      c("Comparison", 
                        "DESeq_model_formula", 

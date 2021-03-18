75126a720cc40c7de06241eae39699b3da812293
Owen Dando <owen.dando@ed.ac.uk>
2021-03-18 09:13:14 +0000

    More conflict resolution.

--- {{cookiecutter.project_name}}/load_packages.R
+++ {{cookiecutter.project_name}}/load_packages.R
@@ -23,6 +23,7 @@ library(patchwork)
 library(conflicted)
 
 conflict_prefer("setdiff", "dplyr")
+conflict_prefer("intersect", "dplyr")
 conflict_prefer("filter", "dplyr")
 conflict_prefer("reduce", "purrr")
 conflict_prefer("rowVars", "matrixStats")

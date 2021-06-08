654a1be28e501769d622047785a8d7f52b62a218
Owen Dando <owen.dando@ed.ac.uk>
2021-06-08 15:00:15 +0100

    Fix cell type legends when > 2 genes.

--- {{cookiecutter.project_name}}/common_functions.R
+++ {{cookiecutter.project_name}}/common_functions.R
@@ -896,46 +896,42 @@ check_cell_type <- function(result_table, fpkm_check_cutoff = 5,
   
   gene_markers <- GENE_MARKERS %>% pull(SPECIES) %>% split(f = GENE_MARKERS$cell_type)
   fpkm_info <- data.frame()
-  p <- ''
   message("Cell type check results are saved in [", GRAPHS_DIR,"]")
   
-  # for(g in split(genes_markers, ceiling(seq_along(genes)/4)) ){
   for (cell_type in gene_markers %>% names()) {
     genes <- gene_markers %>% extract2(cell_type)
+    num_genes <- length(genes)
     
-    for (i in genes) {
-      plot_name <- str_c(cell_type, '_', i)
-      l <- plot_gene_fpkms(i, result_table = result_table, debug = FALSE, print_graph = FALSE, feature_group = CELLTYPE_FEATURE_GROUP, plot_feature = CELLTYPE_PLOT_FEATURE)
+    plot_statement <- ''
+    
+    for (index in seq(num_genes)) {
+      plot_name <- str_c(cell_type, '_', genes[index])
+      l <- plot_gene_fpkms(genes[index], result_table = result_table, debug = FALSE, print_graph = FALSE, feature_group = CELLTYPE_FEATURE_GROUP, plot_feature = CELLTYPE_PLOT_FEATURE)
       l$graph <- l$graph + geom_hline(yintercept = fpkm_check_cutoff, linetype = "dashed", color = "black")
       # if it's the first graph, override the theme to have no legend
       # ensures legend is only on right hand side of graph
-      if (nchar(p) == 0) {
+      if (index < num_genes) {
         l$graph <- l$graph + theme_update(legend.position = "none",
                                           axis.title.x = element_blank(),
                                           axis.text.x = element_blank(),
                                           axis.ticks.x = element_blank())
       }
-
-
+      
+      
       assign(plot_name, l$graph)
       fpkm_info <- l$info %>% mutate(gene_marker_cell_type = cell_type) %>% rbind(fpkm_info)
       
-      ifelse(nchar(p) == 0,
-             p <- plot_name,
-             p <- str_c(p, plot_name, sep = '+')
-      )
+      plot_statement <- ifelse(index == 1, plot_name, plot_statement %>% str_c(plot_name, sep = '+'))
     }
     
     start_plot(str_c("cell_type_check_", cell_type))
     
-    if (length(genes) < 2) {
-      p %>% parse_expr() %>% eval() %>% print()
-    } else {
-      str_c(p, " plot_layout(ncol = 2)", sep = '+') %>% parse_expr() %>% eval() %>% print()
+    if (num_genes > 1) {
+      plot_statement %<>% str_c(" + plot_layout(ncol = ", num_genes, ")") 
     }
+    plot_statement %>% parse_expr() %>% eval() %>% print()
     
     end_plot()
-    p <- ''
   }
   
   ## check fpkms

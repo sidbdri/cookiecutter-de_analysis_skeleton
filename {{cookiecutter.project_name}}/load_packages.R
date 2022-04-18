1f1787aaad67bc5a19f2e7a8311914e0cf860c88
katieemelianova <katie_emelianova@hotmail.com>
2022-04-18 11:32:07 +0100

    remove hardcoded species Add cookiecutter ensembl refernece add another conflict resolution

--- {{cookiecutter.project_name}}/load_packages.R
+++ {{cookiecutter.project_name}}/load_packages.R
@@ -38,6 +38,7 @@ conflict_prefer("parSapply", "parallel")
 conflict_prefer("extract", "magrittr")
 conflict_prefer("strsplit", "base")
 conflict_prefer("cor", "WGCNA")
+conflict_prefer("rename", "dplyr")
 
 sessionInfo()
 
